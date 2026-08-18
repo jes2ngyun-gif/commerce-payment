8895a1384373ccc30d631a7dde6fe54679205d8d
최정윤 <jes2ngyun@gmail.com>
2026-08-18 15:18:18 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/jes2ngyun/commerce_payment/CommercePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.jes2ngyun.commerce_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercePaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jes2ngyun/commerce_payment/CommercePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jes2ngyun.commerce_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
